4f698da8fde4368c6735bec6101fe79d1b69e639
rubaiiyat <abirr1516@gmail.com>
2025-07-01 18:41:25 +0600

    add cart length

--- 06-Add-To-Cart/src/Components/Navbar/Navbar.jsx
+++ 06-Add-To-Cart/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router";
 
 const Navbar = () => {
+  const carts = useSelector((state) => state.carts);
+  console.log(carts);
   return (
     <div className="navbar bg-base-100 shadow-sm justify-between ">
       <div className="mx-0 md:mx-24 ">
@@ -28,7 +31,9 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />{" "}
               </svg>
-              <span className="badge badge-sm indicator-item">0</span>
+              <span className="badge badge-sm indicator-item">
+                {carts.length}
+              </span>
             </div>
           </div>
           <div
@@ -36,7 +41,7 @@ const Navbar = () => {
             className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
           >
             <div className="card-body">
-              <span className="text-lg font-bold">0 Items</span>
+              <span className="text-lg font-bold">{carts.length} Items</span>
               <span className="text-info">Subtotal: $00</span>
               <div className="card-actions">
                 <Link to={"/cart"} className="btn btn-info btn-block">
