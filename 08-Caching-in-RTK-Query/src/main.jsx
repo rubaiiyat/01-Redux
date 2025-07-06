5c787794721493fe41b0eabb2c40d68ec121bebf
rubaiiyat <abirr1516@gmail.com>
2025-07-06 20:03:24 +0600

    create store and add provider

--- 08-Caching-in-RTK-Query/src/main.jsx
+++ 08-Caching-in-RTK-Query/src/main.jsx
@@ -2,13 +2,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Provider } from "react-redux";
+import { Store } from "./Redux/Store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route element={<App></App>}>
-        <Route path="/" element={<div>RTK ADVANCED TOPIC</div>}></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={Store}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App></App>}>
+          <Route path="/" element={<div>RTK ADVANCED TOPIC</div>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
